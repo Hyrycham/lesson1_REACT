9046aca0ab804f1f1c6b9e61d6c6fa8351f11ea9
hyrycham@gmail.com <hyrycham@gmail.com>
2021-06-24 14:28:23 +0300

    begin new function Users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import './App.css';
+import Users  from './components/users/users'
 export default App;
 
 
 function App() {
   return (
       <div>
-        <Hedin title ='t1' description ='d1'/>
-          <Hedin title ='t2' description ='d2'/>
+
+
+
+
 
       </div>
   );
 }
-function Hedin (props) {
-    return(
-        <div>
-            <h1> {props.title} </h1>
-            <p> {props.description} </p>
-        </div>
 
-    )
-}
 
---
+++ src/components/heading.js
@@ -0,0 +1,9 @@
+export default  function Hedin (props) {
+    return(
+        <div>
+            <h1> {props.title} </h1>
+            <p> {props.description} </p>
+        </div>
+
+    )
+}
