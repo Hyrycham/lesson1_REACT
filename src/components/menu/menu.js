3018f1a2a1fe9c2a75fe295d45817001bfe3d406
hyrycham@gmail.com <hyrycham@gmail.com>
2021-06-24 17:41:16 +0300

    add class

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,11 +1,11 @@
-import  ''
+import  'menu.css'
 export default function Menu (props) {
     let {pages} = (props)
 
     return(
         <div>
             <ul>{
-                pages.map(page => <li>{page}</li>)
+                pages.map(page => <li className= 'target'>{page}</li>)
 
 
             }
