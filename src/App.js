2684c3fae9d613bcd10605d0f8d8ddccaa75481f
hyrycham@gmail.com <hyrycham@gmail.com>
2021-06-24 19:55:57 +0300

    add commentsand user

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 // import Users  from './components/users/users'
 import Menu  from './components/menu/menu'
 // import Posts from "./components/posts/posts";
-import Comments from "./components/comments/comments";
+// import Comments from "./components/comments/comments";
 export default App;
 
 
@@ -13,7 +13,10 @@ function App() {
           < Menu  pages={['users page', 'posts page' , 'comments page']}
                  classes= {['tr','point']}
           />
-                 <Comments/>
+          {/*<Users/>*/}
+          {/*<Posts/>*/}
+
+
 
           < Menu  pages={['about', 'team' , 'contact']}
                   classes= {['target','point','asd']}
