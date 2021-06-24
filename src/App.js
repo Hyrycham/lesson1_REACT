233bbb65ad97db4cdfef2304ececaeed73b25e84
hyrycham@gmail.com <hyrycham@gmail.com>
2021-06-24 14:16:59 +0300

    begin new function

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ export default App;
 function App() {
   return (
       <div>
-        <Card/>
-      </div>
+        <Hedin title ='t1' description ='d1'/>
+          <Hedin title ='t2' description ='d2'/>
 
+      </div>
   );
 }
-function Card(){
-    return(<div>ssssssssddddd</div>)
+function Hedin (props) {
+    return(
+        <div>
+            <h1> {props.title} </h1>
+            <p> {props.description} </p>
+        </div>
+
+    )
 }
 
