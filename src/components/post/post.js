22a21e35748cd0c0982bf63c77cd4855d75da947
hyrycham@gmail.com <hyrycham@gmail.com>
2021-06-25 18:10:10 +0300

    add users and posts and style

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,9 +1,10 @@
+import './post.css'
 export default function Post ({item}) {
        return (
         <div>
 
-            <div>  {item.title} </div>
-
+            <div className={'itemBody'}>  {item.body}  </div>
+           <div className={'itemTitle'}>  {item.title}  </div>
         </div>
     )
 }
